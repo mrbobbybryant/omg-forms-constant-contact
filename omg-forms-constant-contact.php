c677a1210ee186d62e82b18b38521d3c6b61bad1
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2017-11-08 12:43:56 -0500

    add logic to default constant contact forms to use rest, bump version

--- omg-forms-constant-contact.php
+++ omg-forms-constant-contact.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( !defined( 'OMG_FORMS_CC_VERSION' ) ) {
-	define( 'OMG_FORMS_CC_VERSION', '0.1.0' );
+	define( 'OMG_FORMS_CC_VERSION', '0.1.1' );
 }
 
 if ( !defined( 'OMG_FORMS_CC_DIR' ) ) {
@@ -18,3 +18,11 @@ require_once OMG_FORMS_CC_DIR . '/includes/helpers.php';
 require_once OMG_FORMS_CC_DIR . '/includes/settings.php';
 
 \OMGForms\CC\Settings\setup();
+
+function constant_contact_force_rest( $args ) {
+	if ( $args['form_type'] === 'constant-contact' ) {
+		$args[ 'rest_api' ] = true;
+	}
+	return $args;
+}
+add_filter( 'omg_form_filter_register_args', 'constant_contact_force_rest' );
