2f48f3694607fe03dd0ee9467617df0e5ac43865
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2017-11-21 15:41:07 -0500

    Update API to work with update core API - Upgrade Addon to use the new version of OMG Forms core. - Endpoints now return the original data they were give or an error. - So endpoint has an additional check to see if the data it receives is an error

--- includes/api.php
+++ includes/api.php
@@ -3,34 +3,69 @@ namespace OMGForms\CC\API;
 
 use Ctct\Components\Contacts;
 use OMGForms\CC\Helpers;
+use OMGForms\Helpers as CoreHelpers;
 
-function save_form_as_constant_contact( $result, $args, $form ) {
+function save_form_as_constant_contact( $args, $form ) {
 
-	if ( $form[ 'form_type' ] === 'constant-contact' ) {
-		$apiKey = get_option( 'cc_api_key' );
-		$apiToken = get_option( 'cc_api_token' );
+	if ( ! CoreHelpers\is_form_type( 'constant-contact', $form ) || is_wp_error( $args ) ) {
+		return $args;
+	}
 
-		if ( empty( $apiKey ) ) {
-			throw new \Exception( 'You must set the API Key before you Constant contact form will work.' );
-		}
+	$apiKey = get_option( 'cc_api_key' );
+	$apiToken = get_option( 'cc_api_token' );
 
-		if ( empty( $apiToken ) ) {
-			throw new \Exception( 'You must set the API Token before you Constant contact form will work.' );
-		}
+	if ( empty( $apiKey ) ) {
+		return CoreHelpers\return_error(
+			'constant-contact-error',
+			'You must set the API Key before you Constant contact form will work.',
+			400
+		);
+	}
 
-		$data = Helpers\prepare_cc_form_fields( $args );
+	if ( empty( $apiToken ) ) {
+		return CoreHelpers\return_error(
+			'constant-contact-error',
+			'You must set the API Token before you Constant contact form will work.',
+			400
+		);
+	}
 
-		$cc = new \Ctct\ConstantContact( get_option( 'cc_api_key' ) );
+	$data = Helpers\prepare_cc_form_fields( $args );
 
-		try {
-			// check to see if a contact with the email address already exists in the account
-			$response = $cc->contactService->getContacts( $apiToken, array( "email" => $data['email_address'] ) );
+	$cc = new \Ctct\ConstantContact( get_option( 'cc_api_key' ) );
 
-			// create a new contact if one does not exist
-			if ( empty( $response->results ) ) {
-				$contact = new Contacts\Contact();
-				$contact->addEmail( $data['email_address'] );
-				$contact->addList( $form['list_id'] );
+	try {
+		// check to see if a contact with the email address already exists in the account
+		$response = $cc->contactService->getContacts( $apiToken, array( "email" => $data['email_address'] ) );
+
+		// create a new contact if one does not exist
+		if ( empty( $response->results ) ) {
+			$contact = new Contacts\Contact();
+			$contact->addEmail( $data['email_address'] );
+			$contact->addList( $form['list_id'] );
+
+			if ( isset( $data['first_name'] ) ) {
+				$contact->first_name = $data['first_name'];
+			}
+
+			if ( isset( $data['last_name'] ) ) {
+				$contact->last_name = $data['last_name'];
+			}
+
+			/*
+			 * The third parameter of addContact defaults to false, but if this were set to true it would tell Constant
+			 * Contact that this action is being performed by the contact themselves, and gives the ability to
+			 * opt contacts back in and trigger Welcome/Change-of-interest emails.
+			 *
+			 * See: http://developer.constantcontact.com/docs/contacts-api/contacts-index.html#opt_in
+			 */
+			$returnContact = $cc->contactService->addContact( $apiToken, $contact, [ 'action_by' => 'ACTION_BY_VISITOR' ] );
+
+			// update the existing contact if address already existed
+		} else {
+			$contact = $response->results[0];
+			if ( $contact instanceof Contacts\Contact ) {
+				$contact->addList($form['list_id']);
 
 				if ( isset( $data['first_name'] ) ) {
 					$contact->first_name = $data['first_name'];
@@ -41,50 +76,26 @@ function save_form_as_constant_contact( $result, $args, $form ) {
 				}
 
 				/*
-				 * The third parameter of addContact defaults to false, but if this were set to true it would tell Constant
-				 * Contact that this action is being performed by the contact themselves, and gives the ability to
+				 * The third parameter of updateContact defaults to false, but if this were set to true it would tell
+				 * Constant Contact that this action is being performed by the contact themselves, and gives the ability to
 				 * opt contacts back in and trigger Welcome/Change-of-interest emails.
 				 *
 				 * See: http://developer.constantcontact.com/docs/contacts-api/contacts-index.html#opt_in
 				 */
-				$returnContact = $cc->contactService->addContact( $apiToken, $contact, [ 'action_by' => 'ACTION_BY_VISITOR' ] );
-
-				// update the existing contact if address already existed
+				$returnContact = $cc->contactService->updateContact( $apiToken, $contact, [ 'action_by' => 'ACTION_BY_VISITOR' ] );
 			} else {
-				$contact = $response->results[0];
-				if ( $contact instanceof Contacts\Contact ) {
-					$contact->addList($form['list_id']);
-
-					if ( isset( $data['first_name'] ) ) {
-						$contact->first_name = $data['first_name'];
-					}
-
-					if ( isset( $data['last_name'] ) ) {
-						$contact->last_name = $data['last_name'];
-					}
-
-					/*
-					 * The third parameter of updateContact defaults to false, but if this were set to true it would tell
-					 * Constant Contact that this action is being performed by the contact themselves, and gives the ability to
-					 * opt contacts back in and trigger Welcome/Change-of-interest emails.
-					 *
-					 * See: http://developer.constantcontact.com/docs/contacts-api/contacts-index.html#opt_in
-					 */
-					$returnContact = $cc->contactService->updateContact( $apiToken, $contact, [ 'action_by' => 'ACTION_BY_VISITOR' ] );
-				} else {
-					$e = new \Ctct\Exceptions\CtctException();
-					$e->setErrors( array( "type", "Contact type not returned" ) );
-					return $e;
-				}
+				$e = new \Ctct\Exceptions\CtctException();
+				$e->setErrors( array( "type", "Contact type not returned" ) );
+				return CoreHelpers\return_form_level_error( $e->getErrors() );
 			}
-
-			// catch any exceptions thrown during the process and print the errors to screen
-		} catch (\Ctct\Exceptions\CtctException $ex) {
-			return $ex;
 		}
+
+		// catch any exceptions thrown during the process and print the errors to screen
+	} catch (\Ctct\Exceptions\CtctException $ex) {
+		return CoreHelpers\return_form_level_error( $ex->getErrors() );
 	}
 
-	return true;
+	return $args;
 }
 
 add_filter( 'omg_forms_save_data', __NAMESPACE__ .  '\save_form_as_constant_contact', 10, 3 );
